5f5806855c3c3408def07cc3ab474a9fd6fc622c
CLM <limin_chu@163.com>
2018-03-08 20:47:22 +0800

    babel编译ES6语法
    babel编译ES6语法

---
+++ ES6语法/模块化/babel-new-part/src/index.js
@@ -0,0 +1,10 @@
+[1, 2, 3].map(item => item + 1)
+
+//执行命令 javascript-advanced-knowledge-point-comb\ES6语法\模块化\babel-new-part>babel src/index.js
+
+// 生成ES5语法
+// "use strict";
+//
+// [1, 2, 3].map(function (item) {
+//   return item + 1;
+// });
