ec82544f039c8159e1be34fc78eccf515d464376
CLM <limin_chu@163.com>
2018-03-13 22:00:27 +0800

    index.js
    index.js

--- ES6语法/Class/JS构造函数/index.js
+++ ES6语法/Class/JS构造函数/index.js
@@ -11,4 +11,12 @@ MathHandle.prototype.add = function() {
 
 var m = new MathHandle(1, 2)
 // console.log(m.add())
-document.getElementById('test').innerHTML = m.add()
+document.getElementById('test').innerHTML = m.add();
+
+//MathHandle的显示原型(prototype)里面有一个constructor构造函数属性 ===  MathHandle它的本身
+//函数都有显示原型
+console.log(MathHandle.prototype.constructor === MathHandle ) //true
+
+// m是new出来的一个实例，所有的对象都有一个__proto__隐式原型
+// m的(__proto__)隐式原型等于MathHandle的(prototype)显示原型
+console.log(m.__proto__ === MathHandle.prototype) //true
